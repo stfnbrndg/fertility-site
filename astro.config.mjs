32c880c6e6bf1aacb85b70914e88bb6cd2ae4c44
Stefan Brundige <stefan.brundige@kub.org>
2025-12-21 20:29:32 -0500

    Fix site URL to fertilitycentral.netlify.app

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://fertility-guide.netlify.app',
+	site: 'https://fertilitycentral.netlify.app',
 	integrations: [sitemap()],
 });
